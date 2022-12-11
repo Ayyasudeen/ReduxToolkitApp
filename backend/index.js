489734f000695225c2dd59599842687233fa9451
Ayyasudeen <63138561+Ayyasudeen@users.noreply.github.com>
2022-12-11 18:31:19 +0530

    using import instead of require
    added "type": "module" in the package.json for ES module support

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+import express, { json } from "express";
+import cors from "cors";
 const app = express();
-const cors = require("cors");
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
 
 app.post("/api/users/:id/update", (req, res) => {
