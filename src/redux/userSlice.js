2ed1442aae814adf2ec4e1fda797fdf5c55a7814
Ayyasudeen <63138561+Ayyasudeen@users.noreply.github.com>
2022-12-17 21:51:04 +0530

    using asyncThunk

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,4 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+// with Thunk 
+
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+ 
+//  users/update is the action name, we can use any name we want 
+export const updateuser2 = createAsyncThunk("users/update", async (user) => {
+    const response = await fetch("http://localhost:8800/api/users/1/update", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      })
+      return response.data;
+})
+
 
 
 export const userSlice = createSlice({
@@ -13,32 +28,62 @@ export const userSlice = createSlice({
         success: false,
         closeSuccess: true
     },
-    reducers: {
-        updateStart: (state) => {
-            state.pending = true;
-            state.closeSuccess = false;
-        },
-        updateSuccess: (state, action) => {
-            state.pending = false;
-            state.userInfo = action.payload;
-            state.error = false;
-            state.success = true;
-            
-        },
-        updateSuccessClose: (state) => {
-            state.pending = false;
-            state.closeSuccess = true;
-            state.success = false;
-        },
-        updateFailure: (state) => {
-            state.pending = false;
-            state.error = true;
-            state.success = false;
-        }
-    }
+    reducers: {}
 });
 
-export const { updateStart, updateSuccess, updateFailure, updateSuccessClose } = userSlice.actions;
+export const {  } = userSlice.actions;
 export default userSlice.reducer;
+
+
+
+
+
+
+
+
+// // without Thunk 
+
+// import { createSlice } from '@reduxjs/toolkit';
+
+
+// export const userSlice = createSlice({
+//     name: "user",
+//     initialState: {
+//         userInfo: {
+//             name: "Ayyas", 
+//             email: "ayyas@email.com"
+//         },
+//         pending: false,
+//         error: false,
+//         success: false,
+//         closeSuccess: true
+//     },
+//     reducers: {
+//         updateStart: (state) => {
+//             state.pending = true;
+//             state.closeSuccess = false;
+//         },
+//         updateSuccess: (state, action) => {
+//             state.pending = false;
+//             state.userInfo = action.payload;
+//             state.error = false;
+//             state.success = true;
+            
+//         },
+//         updateSuccessClose: (state) => {
+//             state.pending = false;
+//             state.closeSuccess = true;
+//             state.success = false;
+//         },
+//         updateFailure: (state) => {
+//             state.pending = false;
+//             state.error = true;
+//             state.success = false;
+//         }
+//     }
+// });
+
+// export const { updateStart, updateSuccess, updateFailure, updateSuccessClose } = userSlice.actions;
+// export default userSlice.reducer;
 
 
